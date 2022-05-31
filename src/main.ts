de03696289be86393f02dc9d840cc185ea8cf3d5
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2022-05-31 22:27:56 +0200

    refactored

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: true
-  });
-  
-  app.enableVersioning({
-    type: VersioningType.URI,
-    defaultVersion: "1"
-  })
-
-  app.enableCors()
-  
+  const app = await NestFactory.create(AppModule, { cors: true, abortOnError: false });
+  app.enableVersioning({ type: VersioningType.URI, defaultVersion: "1" });
+  app.enableCors();
   await app.listen(3001);
 }
 
