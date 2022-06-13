154c0e4d2edc427f3592259440feda61bd1ba6d7
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2022-06-13 13:08:13 +0200

    added configurable port

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import { VersioningType } from '@nestjs/common';
+import { Logger, VersioningType } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -6,7 +7,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true, abortOnError: false });
   app.enableVersioning({ type: VersioningType.URI, defaultVersion: "1" });
   app.enableCors();
-  await app.listen(3001);
+
+  const config = app.get(ConfigService);
+  const port = config.get<number>("PORT") || 3001
+  const logger = new Logger("Bootstrap");
+  
+  await app.listen(port).then(() => {
+    logger.log(`Soundcore API now listening for requests on port ${port}`);
+  });
 }
 
 bootstrap();
